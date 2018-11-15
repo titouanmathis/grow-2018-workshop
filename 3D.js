123a354727a3f2ebfcbd1ae10a670a10690e5309
Titouan Mathis <titouan@studiometa.fr>
2018-11-15 10:12:07 +0100

    [3D.js] 2018.11.15-10.12.07

--- 3D.js
+++ 3D.js
@@ -16,6 +16,23 @@ const settings = {
 };
 
 const sketch = ({ context }) => {
+  const createGrid = (count = 3) => {
+    const points = [];
+
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        for (let z = 0; z < count; z++) {
+          let u = count <= 1 ? 0.5 : x / (count - 1);
+          let v = count <= 1 ? 0.5 : y / (count - 1);
+          let w = count <= 1 ? 0.5 : z / (count - 1);
+          points.push([u, v, w]);
+        }
+      }
+    }
+
+    return points;
+  };
+
   // Create a renderer
   const renderer = new THREE.WebGLRenderer({
     context,
@@ -26,7 +43,7 @@ const sketch = ({ context }) => {
 
   // Setup a camera
   const camera = new THREE.PerspectiveCamera(45, 1, 0.01, 100);
-  camera.position.set(2, 2, -4);
+  camera.position.set(20, 20, -4);
   camera.lookAt(new THREE.Vector3());
 
   // Setup camera controller
@@ -35,21 +52,34 @@ const sketch = ({ context }) => {
   // Setup your scene
   const scene = new THREE.Scene();
 
-  const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshPhysicalMaterial({
-      color: 'white',
-      roughness: 0.75,
-      flatShading: true,
-    })
-  );
-  scene.add(mesh);
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshPhysicalMaterial({
+    color: '#fff',
+    roughness: 0.75,
+    flatShading: true,
+  });
+  const grid = createGrid();
+  const meshes = [];
+
+  grid.forEach(point => {
+    const [u, v, w] = point;
+    const x = u * 3;
+    const y = v * 3;
+    const z = w * 3;
+    const mesh = new THREE.Mesh(geometry, material);
+    const position = new THREE.Vector3(x, y, z);
+    mesh.position.x = position.x;
+    mesh.position.y = position.y;
+    mesh.position.z = position.z;
+    meshes.push(mesh);
+    scene.add(mesh);
+  });
 
   // Specify an ambient/unlit colour
-  scene.add(new THREE.AmbientLight('#59314f'));
+  scene.add(new THREE.AmbientLight('#fff'));
 
   // Add some light
-  const light = new THREE.PointLight('#45caf7', 1, 15.5);
+  const light = new THREE.PointLight('#fff', 1, 15.5);
   light.position.set(2, 2, -4).multiplyScalar(1.5);
   scene.add(light);
 
@@ -64,7 +94,9 @@ const sketch = ({ context }) => {
     },
     // Update & render your scene here
     render({ time }) {
-      mesh.rotation.y = time * ((10 * Math.PI) / 180);
+      meshes.forEach(mesh => {
+        mesh.rotation.y = time * ((10 * Math.PI) / 90);
+      });
       controls.update();
       renderer.render(scene, camera);
     },
