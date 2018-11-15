64e24b1d538424461a9015ad7dce89a133e6f4c2
Titouan Mathis <titouan@studiometa.fr>
2018-11-15 16:33:55 +0100

    [sphere.js] 2018.11.15-16.33.55

--- sphere.js
+++ sphere.js
@@ -1,5 +1,6 @@
 const canvasSketch = require('canvas-sketch');
 const glsl = require('glslify');
+const load = require('load-asset');
 
 // Ensure ThreeJS is in global scope for the 'examples/'
 global.THREE = require('three');
@@ -13,17 +14,19 @@ const settings = {
   // Get a WebGL canvas rather than 2D
   context: 'webgl',
   dimensions: [512, 512],
+  duration: 6,
   // Turn on MSAA
   attributes: { antialias: true },
+  params: {
+    image: ['./assets/thomas.png'],
+  },
 };
 
-const sketch = ({ context, canvas }) => {
+const sketch = async ({ context, canvas }) => {
   // Create a renderer
   const renderer = new THREE.WebGLRenderer({
     context,
   });
-
-  canvas.style.borderRadius = '50%';
 
   // WebGL background color
   renderer.setClearColor('#000', 1);
@@ -39,12 +42,20 @@ const sketch = ({ context, canvas }) => {
   // Setup your scene
   const scene = new THREE.Scene();
 
+  const image = await load('./assets/thomas.png');
+
+  const texture = new THREE.Texture(image);
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+  texture.needsUpdate = true;
+
   const mesh = new THREE.Mesh(
     new THREE.SphereGeometry(1, 32, 32),
     new THREE.ShaderMaterial({
       uniforms: {
         time: { value: 0 },
         aspect: { value: 0 },
+        image: { value: texture },
       },
       vertexShader: glsl(/* syntax: glsl */ `
         #pragma glslify: noise = require('glsl-noise/simplex/4d');
@@ -61,6 +72,7 @@ const sketch = ({ context, canvas }) => {
         uniform float aspect;
         uniform vec2 pointer;
         varying vec2 vUv;
+        uniform sampler2D image;
 
         #pragma glslify: noise = require('glsl-noise/simplex/3d');
         #pragma glslify: hsl2rgb = require('glsl-hsl2rgb');
@@ -74,12 +86,16 @@ const sketch = ({ context, canvas }) => {
           hue = mod(hue + time * 0.05, 1.0);
 
           vec3 color = hsl2rgb(hue, 0.5, 0.5);
-          gl_FragColor = vec4(vec3(color), 1.0);
+          gl_FragColor = vec4(texture2D(image, vUv).rgb, 1.0);
         }
       `),
     })
   );
   scene.add(mesh);
+
+  mesh.rotation.x = 0.4;
+  mesh.rotation.y = 1;
+  mesh.scale.multiplyScalar(1.2);
 
   // Specify an ambient/unlit colour
   scene.add(new THREE.AmbientLight('#59314f'));
@@ -100,6 +116,7 @@ const sketch = ({ context, canvas }) => {
     },
     // Update & render your scene here
     render({ time, width, height }) {
+      // mesh.rotation.y = Math.sin(time * 0.5);
       mesh.material.uniforms.time.value = time;
       mesh.material.uniforms.aspect.value = width / height;
       controls.update();
